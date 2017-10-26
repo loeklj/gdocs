482523cfd836a700d9f790d08cd8eab7ebbd854a
Loek Nova <loek@neednova.com>
2017-10-26 11:20:47 -0700

    Add edit and sync

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -15,12 +15,56 @@ const ENDPOINTS = {
 	},
 }
 
-$(document).ready(() => {
-	fetch(ENDPOINTS.SYNC.url, ENDPOINTS.SYNC.options({ headers: undefined }))
-	.then((data) => {
-		console.log(data);
-	})
+const TIMERS = {
+	EDIT: 1000,
+}
+
+// avoid re-traveling the dom e.g.
+const cache = {};
+
+// simple cache for timers
+const timerCache = {
+	edit: [],
+};
+
+const sync = () => fetch(ENDPOINTS.SYNC.url, ENDPOINTS.SYNC.options({ headers: undefined }))
 	.catch((err) => {
 		console.log(err);
 	});
+
+const checkEdit = (newText) => {
+	if (typeof newText === 'undefined' || newText == null) return;
+	if (!cache.hasOwnProperty('text')) {
+		cache.text = ''
+	}
+
+	// check for changes
+	if (cache.text !== newText) {
+		
+	}
+}
+
+
+const setup = () => {
+	// get doc element
+	const novaDoc = docoument.getElementById('novadoc');
+	cache.novaDoc = novaDoc;
+
+	// start edit timer instantly
+	const timerEditID = setInterval(() => {
+		checkEdit(cache.novaDoc ? cache.novaDoc.innterHTML : undefined);
+	}, TIMERS.EDIT);
+
+	timerCache.edit.push(timerEditID);
+};
+
+$(document).ready(() => {
+	setup();
+	sync().then((data) => {
+		console.log(data);
+		if (data) {
+			DOMCache.novaDoc.innerHTML = data;
+			console.log('Updated!');
+		}
+	})
 })
