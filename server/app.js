decbc263943e0dc1a9f2025e3fd4e91a9fe1ed72
Loek Nova <loek@neednova.com>
2017-10-26 10:57:59 -0700

    Add middleware dummy

--- server/app.js
+++ server/app.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import http from 'http';
-import routes from './routes';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'))
-
-// attach routes
-routes(app);
 
 app.listen(3004, () => console.log('GDocs app listening on port 3004!'));
